31ce26f21d5acdc51c722575810b4523a5f006b1
johnny-hyde <mutalijohnson34@gmail.com>
2024-04-03 11:40:47 +0300

    more on footer

--- src/components/Movies/MoviesDetails/index.jsx
+++ src/components/Movies/MoviesDetails/index.jsx
@@ -15,39 +15,41 @@ export default function MoviesDetails() {
             style={{
                 backgroundImage: `url(${imgUrl}${movie.backdrop_path})`
             }}>
-            < div className="movie-container__header" >
-                < div className="header__main" >
-                    <h1 className="title">{movie.title}</h1>
-                    <div className="release-and-cast">
-                        <span className="release">{movie.release_date}</span>
-                        <button type="submit" className="btn">watch trailer</button>
-                    </div>
+            <div className="movie-container__main">
+                <div className="movie-container__header">
+                    <div className="header__main">
+                        <h1 className="title">{movie.title}</h1>
+                        <div className="release-and-cast">
+                            <span className="release">{movie.release_date}</span>
+                            <button type="submit" className="btn">watch trailer</button>
+                        </div>
+                    </div >
+                    <div className="header__id">{movie.id}</div>
                 </div >
-                <div className="header__id">{movie.id}</div>
-            </div >
-            <div className="hr"></div>
-            <div className="movie-container__details">
-                <div className="thumbnail img-container">
-                    <img
-                        src={imgUrl + movie.poster_path}
-                        className="img thumbnail"
-                    />
-                </div>
-                <div className="review-and-genre">
-                    <p className="review">{movie.overview}</p>
-                    <div className="genres">{
-                        movie.genres.map(genre => (
-                            <span className="genre-btn" key={genre.id}> {genre.name}</span>
-                        ))
-                    }</div>
-                </div>
-                <div className="rating-and-vote">
-                    <div className="rating">{movie.vote_average}</div>
-                    <div className="vote">{movie.popularity}</div>
+                <div className="hr"></div>
+                <div className="movie-container__details">
+                    <div className="thumbnail img-container">
+                        <img
+                            src={imgUrl + movie.poster_path}
+                            className="img thumbnail"
+                        />
+                    </div>
+                    <div className="review-and-genre">
+                        <p className="review">{movie.overview}</p>
+                        <div className="genres">{
+                            movie.genres.map(genre => (
+                                <span className="genre-btn" key={genre.id}> {genre.name}</span>
+                            ))
+                        }</div>
+                    </div>
+                    <div className="rating-and-vote">
+                        <div className="rating">{movie.vote_average}</div>
+                        <div className="vote">{movie.popularity}</div>
+                    </div>
                 </div>
             </div>
             <Footer />
-        </div >
+        </div>
     )
 }
 
