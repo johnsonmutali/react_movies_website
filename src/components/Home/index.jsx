af4ec1356b9545e75fcb1f88dba56741a9bd6f77
johnny-hyde <mutalijohnson34@gmail.com>
2024-04-05 13:42:49 +0300

    added swiper.js library for carousels

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,35 +2,24 @@ import "./index.scss"
 //pages
 import Footer from "../Footer"
 
+//swiper
+import { Swiper, SwiperSlide } from "swiper/react"
+import { EffectCoverf} from "swiper/react"
+import "swiper/scss"
 
 const Home = () => {
     return (
         <>
             <div className="home">
-                <div className="text-area">
-                    <h1 className="text-area__title">
-                        filmonger
-                    </h1>
-                    <p className="">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at erat tincidunt
-                        ante ultricies mollis at bibendum ligula. Donec quis velit finibus, tempus lacus
-                        nec, eleifend orci. Nam sit amet nisl sem. Donec id commodo erat. Mauris
-                        porttitor orci ac lectus aliquam rutrum. Aliquam erat volutpat. Phasellus vitae
-                        fermentum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                        at erat tincidunt ante ultricies mollis at bibendum ligula. Donec quis velit
-                        finibus, tempus lacus nec, eleifend orci. Nam sit amet nisl sem. Donec id
-                        commodo erat. Mauris porttitor orci ac lectus aliquam rutrum. Aliquam erat
-                        volutpat. Phasellus vitae fermentum nisi. Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit. Fusce at erat tincidunt ante ultricies mollis at
-                        bibendum ligula. Donec quis velit finibus, tempus lacus nec, eleifend orci. Nam
-                        sit amet nisl sem. Donec id commodo erat. Mauris porttitor orci ac lectus
-                        aliquam rutrum. Aliquam erat volutpat. Phasellus vitae fermentum nisi.
-                    </p>
-                </div>
+                <Swiper spaceBetween={50} slidesPerView={3} > <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                </Swiper>
             </div>
             <Footer />
-            </>
-                )
+        </>
+    )
 }
 
-                export default Home
+export default Home
